89cdf177a777d06671717e8321f59ca368499a63
Marta Fiorencis <marfioren@foi.hr>
2021-05-21 12:40:35 +0200

    arduino

--- src/app/form-ArduinoData/form-arduino-data.component.ts
+++ src/app/form-ArduinoData/form-arduino-data.component.ts
@@ -1,22 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { ArduinoFirebase } from '../security/Arduino-firebase';
 import { ArduinoDataService } from '../security/arduino-data.service';
-import {UserService} from '../security/user.service';
 @Component({
   selector: 'app-form-arduino-data',
   templateUrl: './form-arduino-data.component.html',
   styleUrls: ['./form-arduino-data.component.css']
 })
 export class FormArduinoDataComponent implements OnInit {
-  TemperatureCurr: any[]=[];
-
+  TemperatureCurrArray: any[]=[];
+  TemperatureHistArray: any[]=[];
+  HumidityCurrArray: any[]=[];
+  TemperatureCurr: string;
+  HumidityCurr: string;
+  interval: any;
   constructor(private arduinoDataService: ArduinoDataService) { }
 
   ngOnInit(): void {
+    this.TemperatureCurr=" ";
+    this.HumidityCurr=" ";
+    this.updateData();
+    this.interval = setInterval(() => {
+      this.updateData();
+    }, 10000);
+  }
+  updateData(){
     this.arduinoDataService.fillCurrTempData().then(son => {
-      this.TemperatureCurr=this.arduinoDataService.getCurrTemp();
-      console.log(this.TemperatureCurr);
+      this.TemperatureCurrArray=this.arduinoDataService.getCurrTemp();
+      this.TemperatureCurr=this.TemperatureCurrArray[0].data;
     })
+    this.arduinoDataService.fillCurrHumData().then(son => {
+      this.HumidityCurrArray=this.arduinoDataService.getCurrHum();
+      this.HumidityCurr=this.HumidityCurrArray[0].data;
+    })
+  }
+
+  getHistoryData(){
+    this.arduinoDataService.fillHistTempData().then(son => {
+      this.TemperatureHistArray=this.arduinoDataService.getHistTemp();
+      for(var i=0; i<this.TemperatureHistArray.length;i++){
+        console.log(this.TemperatureHistArray[i].time);
+      }
+
+    })
+
+  }
+
+  ShowHistory(){
+
+    this.getHistoryData();
   }
 
 }
--- src/app/security/arduino-data.service.ts
+++ src/app/security/arduino-data.service.ts
@@ -27,8 +27,55 @@ export class ArduinoDataService {
 
   }
   getCurrTemp(){
+
     return this.TemperatureCurr;
   }
+
+  //current humidity
+  fillCurrHumData() {
+    this.HumidityCurr.length=0;
+    return this.getDataFromFirebase("HumCurr")
+      .then((ss) => {
+          ss.docs.forEach((doc) => {
+            this.HumidityCurr.push(doc.data());
+          });
+        }
+      )
+      .catch(err => {
+        console.log(err);
+      });
+
+  }
+  getCurrHum(){
+    return this.HumidityCurr;
+  }
+
+  //get temperature history data
+  fillHistTempData() {
+    this.TemperatureHistory.length=0;
+    return this.getDataFromFirebase("TempHistory")
+      .then((ss) => {
+          ss.docs.forEach((doc) => {
+            this.TemperatureHistory.push(doc.data());
+          });
+        }
+      )
+      .catch(err => {
+        console.log(err);
+      });
+
+  }
+  getHistTemp(){
+    this.TemperatureHistory= this.TemperatureHistory.sort((t1, t2) => {
+      const time1 = Number(t1.timestamp);
+      const time2 = Number(t2.timestamp);
+      if (time1 > time2) { return 1; }
+      if (time1 < time2) { return -1; }
+      return 0;
+    });
+    return this.TemperatureHistory;
+  }
+  //retrieves data from database
   getDataFromFirebase(coll: string){
     return this.angularFirestore
       .collection(coll)
